3bd559e346138ffc7eb77aced16a5e20d7ea9013
Fabio Pereira da Silva <fabioegel@gmail.com>
2020-07-27 20:48:09 -0300

    Adding documentation to docs

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"desafiointelie","l":"CommandParser"},{"p":"desafiointelie","l":"Connector"},{"p":"desafiointelie","l":"Main"},{"p":"desafiointelie","l":"OpenFile"},{"p":"desafiointelie","l":"Parser"}]
